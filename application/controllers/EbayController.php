6cb6eade65015cea4b1a68950519bfb6cc566041
Deepak <dpkydv.jobs@gmail.com>
2013-12-29 00:07:38 +0530

    second commit

---
+++ application/controllers/EbayController.php
@@ -0,0 +1,20 @@
+<?php
+require_once "BaseController.php";
+class EbayController extends BaseController
+{
+
+    public function init()
+    {
+        /* Initialize action controller here */         
+        $this->menuItemActive("ebay");
+        $this->_helper->layout->enableLayout();
+    }
+
+    public function indexAction()
+    {
+        // action body
+    }	
+
+
+}
+
