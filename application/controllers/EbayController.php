fc4fa2467d6ec4a1b4b332466c659f445b4753f3
Deepak <dpkydv.jobs@gmail.com>
2013-12-29 02:13:57 +0530

    permissions changed

--- application/controllers/EbayController.php
+++ application/controllers/EbayController.php
@@ -13,6 +13,22 @@ class EbayController extends BaseController
     public function indexAction()
     {
         // action body
+        try{
+            $phpLiveDocx = new Zend_Service_LiveDocx_MailMerge();     
+            $phpLiveDocx->setUsername('deep7178')
+                        ->setPassword('ramesh45');            
+            $phpLiveDocx->setLocalTemplate('./template.docx');            
+            /*$phpLiveDocx->assign('software', 'Magic Graphical Compression Suite v1.9')
+                        ->assign('licensee', 'Henry Döner-Meyer')
+                        ->assign('company',  'Co-Operation');             
+            $phpLiveDocx->createDocument();             
+            $document = $phpLiveDocx->retrieveDocument('pdf');             
+            file_put_contents('document.pdf', $document);    */
+        }
+        catch(Exception $e){
+            echo $e->getMessage();
+        }
+        
     }	
 
 
